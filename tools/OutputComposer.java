283e23c16c38870c4057e2e1e8f077ea33bdb2bc
chaotic-existential <Ermakova_ib2.19@st.ithub.ru>
2022-01-13 10:37:34 +0300

    Переоформление классов в пакетах Controllers, Tools

---
+++ tools/OutputComposer.java
@@ -0,0 +1,21 @@
+package tools;
+
+public class OutputComposer {
+
+    // todo - придумать нормальное имя класса
+
+    protected String text2optionPlate() {
+        // todo Помещение текста в рамочку
+        return null;
+    }
+
+    protected String coloringDeactivatedPlate() {
+        // todo Перекрашивание недоступных опций в серый
+        return null;
+    }
+
+    protected String generatingRequirementText() {
+        // todo Красненький текст для недоступных опций
+        return null;
+    }
+}
